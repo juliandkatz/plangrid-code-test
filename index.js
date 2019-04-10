2a8068df6ab52d8dc1f73c8a9fe0dfde831450c3
Julian Katz <juliankatz1991@gmail.com>
2019-04-10 09:33:08 -0700

    added header check and curl scripts

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  if (req.accepts('application/json')) {
+    res.json({ message: 'Good morning' })
+  } else {
+    res.send('<p>Hello, World</p>')
+  }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
